ed50a90bdb81a71d1d3f1a2de712fe466141a410
denis-mahei <denmahei@gmail.com>
2025-04-14 19:05:28 +0300

    fix calc

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import 'modern-normalize';
 import App from './components/App/App.jsx';
 import './index.css';
+import '@denis-mahei/custom-normalizer/src/custom-normalizer.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
